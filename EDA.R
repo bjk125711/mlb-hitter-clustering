a6cd219cbd6849155087735dde41b6380edec2a8
bjk125711 <bennettkellogg@gmail.com>
2020-04-05 20:03:22 -0700

    tables added (v3)

--- EDA.R
+++ EDA.R
@@ -7,6 +7,8 @@
 
 library(tidyverse)
 library(corrplot)
+library(factoextra)
+library(reactable)
 
 # Data Import -------------------------------------------------------------
 
@@ -54,9 +56,59 @@ y <- ds %>%
   select(exit_velocity_avg, swing_percent, whiff_percent, pull_percent:opposite_percent, flyballs_percent, linedrives_percent, groundballs_percent,
          popups_percent, solidcontact_percent:hard_hit_percent, launch_angle_avg:barrel_batted_rate) %>% 
   scale(center = TRUE, scale = TRUE)
-km_out_y <- kmeans(y, 6, nstart = 20)
+
+km_out_y <- kmeans(y, 7, nstart = 20)
 km_out_y
+
+fviz_cluster(km_out_y, data = y, stand = TRUE)
+
 ds_5_cluster <- cbind(ds, km_out_y$cluster)
 ds_5_cluster %>% 
   select(`km_out_y$cluster`, last_name, first_name, b_k_percent:b_total_bases) %>% 
   arrange(`km_out_y$cluster`, desc(on_base_plus_slg))
+
+ds_5_cluster %>% 
+  filter(last_name == "Fletcher")
+
+
+# Making a table for cluster outputs to be able to easily read them
+# This needs some extra work for getting the conditional formatting worked in
+
+# Excel-inspired 3-color scale
+GnYlRd <- function(x) rgb(colorRamp(c("#63be7b", "#ffeb84", "#f8696b"))(x), maxColorValue = 255)
+
+reactable(t(round(km_out_y$centers,3)), showSortIcon = TRUE, defaultPageSize = 19)
+
+dimnames <- list(start(nottem)[1]:end(nottem)[1], month.abb)
+temps <- matrix(nottem, ncol = 12, byrow = TRUE, dimnames = dimnames)
+
+# Excel-inspired 3-color scale
+GnYlRd <- function(x) rgb(colorRamp(c("#63be7b", "#ffeb84", "#f8696b"))(x), maxColorValue = 255)
+
+reactable(
+  temps,
+  defaultColDef = colDef(
+    style = function(value) {
+      if (!is.numeric(value)) return()
+      normalized <- (value - min(nottem)) / (max(nottem) - min(nottem))
+      color <- GnYlRd(normalized)
+      list(background = color)
+    },
+    format = colFormat(digits = 1),
+    minWidth = 50
+  ),
+  columns = list(
+    .rownames = colDef(name = "Year", sortable = TRUE, align = "left")
+  ),
+  bordered = TRUE
+)
+
+# Hierarchical Clustering -------------------------------------------------
+
+# David Fletcher makes this not work. He is a singleton when there are very few clusters (~5 in complete linkage). He separates out nearly
+# immediately in hierarchical clustering methods other than complete linkage.
+# He is a contact hitter with no power. He almost never strikes out of walks, swings roughly once an at bat, puts the ball in play with weak contact
+# (2.5 sd above average for flares / burners)
+# When removed, the clusters make more sense but I will not remove him because, although he is an outlier, there is nothing "wrong" about his data
+hc.complete <- hclust(dist(y), method = "complete")
+plot(hc.complete, main = "Complete Linkage", xlab = "", sub = "", cex = 0.9)
